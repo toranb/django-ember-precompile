c0c4ada23b6e941acc9f4930d4a1dd90a41bd21a
Austin Morton <amorton@eaglabs.com>
2013-04-11 13:31:45 -0400

    change to use path.sep instead of hardcoded path separator

--- tests/cli.spec.js
+++ tests/cli.spec.js
@@ -1,4 +1,5 @@
 require('../lib/cli');
+var path = require('path');
 
 describe("CommandLineParser Tests", function() {
 
@@ -25,31 +26,36 @@ describe("CommandLineParser Tests", function() {
 
   it("halt not invoked when valid filepath passed in", function() {
     var haltSpy = spyOn(Cli.prototype, 'haltProcessWithUsage');
-    var sut = new Cli({args:['node', 'node_modules/django-ember-precompile/bin/django-ember-precompile', 'file-system/app/templates/foo.handlebars']});
+    var tpl = path.join('file-system', 'app', 'templates', 'foo.handlebars');
+    var sut = new Cli({args:['node', 'node_modules/django-ember-precompile/bin/django-ember-precompile', tpl]});
     sut.parseCommandLineArgs();
     expect(haltSpy).not.toHaveBeenCalledWith();
   });
 
   it("returns templateName without handlebars extension when valid filepath passed in", function() {
-    var sut = new Cli({args:['node', 'node_modules/django-ember-precompile/bin/django-ember-precompile', 'file-system/app/templates/foo.handlebars']});
+    var tpl = path.join('file-system', 'app', 'templates', 'foo.handlebars');
+    var sut = new Cli({args:['node', 'node_modules/django-ember-precompile/bin/django-ember-precompile', tpl]});
     result = sut.parseCommandLineArgs();
     expect(result['name']).toEqual('foo');
   });
 
   it("returns template content when valid filepath passed in and it exists on the filesystem", function() {
-    var sut = new Cli({args:['node', 'node_modules/django-ember-precompile/bin/django-ember-precompile', 'file-system/app/templates/foo.handlebars']});
+    var tpl = path.join('file-system', 'app', 'templates', 'foo.handlebars');
+    var sut = new Cli({args:['node', 'node_modules/django-ember-precompile/bin/django-ember-precompile', tpl]});
     result = sut.parseCommandLineArgs();
     expect(result['content']).toEqual('{{outlet}}\n');
   });
 
   it("returns templateName without handlebars extension for nested template", function() {
-    var sut = new Cli({args:['node', 'node_modules/django-ember-precompile/bin/django-ember-precompile', 'file-system/app/templates/tables/index.handlebars']});
+    var tpl = path.join('file-system', 'app', 'templates', 'tables', 'index.handlebars');
+    var sut = new Cli({args:['node', 'node_modules/django-ember-precompile/bin/django-ember-precompile', tpl]});
     result = sut.parseCommandLineArgs();
     expect(result['name']).toEqual('tables/index');
   });
 
   it("returns template content for nested template", function() {
-    var sut = new Cli({args:['node', 'node_modules/django-ember-precompile/bin/django-ember-precompile', 'file-system/app/templates/tables/index.handlebars']});
+    var tpl = path.join('file-system', 'app', 'templates', 'tables', 'index.handlebars');
+    var sut = new Cli({args:['node', 'node_modules/django-ember-precompile/bin/django-ember-precompile', tpl]});
     result = sut.parseCommandLineArgs();
     expect(result['content']).toEqual('{{outlet}}\n');
   });
